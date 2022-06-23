9e1f96c2a2335595647a8bb62d0cbea46e039ce8
cyun-404 <73474866+cyun-404@users.noreply.github.com>
2022-06-23 16:23:19 +0900

    Update final_servo.py

--- final_servo.py
+++ final_servo.py
@@ -12,13 +12,13 @@ GPIO.output(16,GPIO.LOW)
 p=GPIO.PWM(16,50)
 p.start(0)
 cnt=0
-# Create the haar cascade
+
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 while(True):
 	# Capture frame-by-frame
 	ret, frame = cap.read()
 
-	# Our operations on the frame come here
+	
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	# Detect faces in the image
@@ -32,9 +32,7 @@ while(True):
 
 	print("Found {0} faces!".format(len(faces)))
 		
-	#else GPIO.output(signalPIN, GPIO.LOW)
-    
-	# Draw a rectangle around the faces
+	
 	for (x, y, w, h) in faces:
 		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 	if ((len(faces))!=0):
@@ -42,19 +40,16 @@ while(True):
 		p.ChangeDutyCycle(4.5)
 		print("BUZZ,LED ON")
 		time.sleep(2.0)
-		#p.ChangeDutyCycle(12.5)
+		
 	else :
 		#GPIO.output(16,GPIO.LOW)
 		p.ChangeDutyCycle(1.5)
-		print("BUZZ,LED OFF")
-		#p.ChangeDutyCycle(2.5)
+		print("BUZZ,LED OFF")		
 		
-	# Display the resulting frame
 	cv2.imshow('frame', frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
-# When everything done, release the capture
 cap.release()
 GPIO.cleanup()
 cv2.destroyAllWindows()
